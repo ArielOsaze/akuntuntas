331e83c35c062990429ea8510e63a6179b510e33
ArielOsaze <arielbudi4@gmail.com>
2026-09-26 11:57:53 +0700

    Naikkan versi ke 1.2.1

--- tools/tandatangani.py
+++ tools/tandatangani.py
@@ -43,7 +43,7 @@ import sys
 
 AKAR = pathlib.Path(__file__).resolve().parent.parent
 SETELAN = AKAR / "sertifikat.json"
-INSTALLER = AKAR / "installer_output" / "AkunTuntas-1.2.0-Setup.exe"
+INSTALLER = AKAR / "installer_output" / "AkunTuntas-1.2.1-Setup.exe"
 APLIKASI = AKAR / "dist" / "AkunTuntas" / "AkunTuntas.exe"
 
 
